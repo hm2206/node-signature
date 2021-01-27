65b3dedd9e5d6d7b610f2c51c6f8b5a2ad6ac422
Hans <twd2206@gmail.com>
2021-01-26 19:07:40 -0500

    add is_vertical

--- src/test.js
+++ src/test.js
@@ -2,10 +2,13 @@ const Signer = require('./Signer');
 const path = require('path');
 const fs = require('fs');
 
-let pfx = path.join(__dirname, "apples.pfx");
+let pfx = path.join(__dirname, "texas.pfx");
 let pdf = path.join(__dirname, "example.pdf");
 let salida = path.join(__dirname, "signature_example.pdf");
 
-Signer(pfx, "apples", pdf, salida)
+Signer(pfx, "joserogelio", pdf, salida, {
+        position: 39,
+        page: 2
+    })
     .then(res => console.log(res))
     .catch(err => console.log(err));
