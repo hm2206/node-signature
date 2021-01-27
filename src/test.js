3853037daee3abde6171f639b898bc92ce1462f0
Hans <twd2206@gmail.com>
2021-01-26 19:27:27 -0500

    fixed orientación

--- src/test.js
+++ src/test.js
@@ -6,9 +6,24 @@ let pfx = path.join(__dirname, "texas.pfx");
 let pdf = path.join(__dirname, "example.pdf");
 let salida = path.join(__dirname, "signature_example.pdf");
 
-Signer(pfx, "joserogelio", pdf, salida, {
+const test1 = () => {
+    Signer(pfx, "joserogelio", pdf, salida, {
         position: 39,
         page: 2
     })
     .then(res => console.log(res))
     .catch(err => console.log(err));
+}
+
+
+const test2 = () => {
+    Signer(pfx, "joserogelio", pdf, salida, {
+        position: 43,
+        page: 1
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+}
+
+
+test1();
